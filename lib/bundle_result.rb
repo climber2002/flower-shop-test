f50415f40df216a3533a6dc0e58243257f9efeb0
andy.wang <andy.wang@blockchaintech.net.au>
2019-01-15 21:43:28 +1100

    Add can_create_bundle?, create_bundle?, all_bundled? to class BundleResult

--- lib/bundle_result.rb
+++ lib/bundle_result.rb
@@ -1,9 +1,42 @@
 class BundleResult
-  attr_reader :total_flowers, :remaining_flowers, :flower_code
+  attr_reader :total_flowers, :remaining_flowers, :flower_code, :bundles
 
   def initialize(total_flowers, flower_code)
     @total_flowers = total_flowers
     @remaining_flowers = total_flowers
     @flower_code = flower_code
+    @bundles = {}
+  end
+
+  def can_create_bundle?(bundle_rule)
+    remaining_flowers >= bundle_rule.quantity
+  end
+
+  def create_bundle(bundle_rule)
+    unless can_create_bundle?(bundle_rule)
+      raise ArgumentError,
+            "Can't fulfill the bundle, bundle quantity: #{bundle_rule.quantity}, remaining: #{remaining_flowers}"
+    end
+
+    add_bundle(bundle_rule)
+    update_remaining(bundle_rule)
+  end
+
+  def all_bundled?
+    remaining_flowers.zero?
+  end
+
+  private
+
+  def add_bundle(bundle_rule)
+    if bundles.key?(bundle_rule)
+      bundles[bundle_rule] += 1
+    else
+      bundles[bundle_rule] = 1
+    end
+  end
+
+  def update_remaining(bundle_rule)
+    @remaining_flowers -= bundle_rule.quantity
   end
 end
--- spec/unit/bundle_result_spec.rb
+++ spec/unit/bundle_result_spec.rb
@@ -1,9 +1,86 @@
 require 'bundle_result'
 
 describe BundleResult do
-  subject { BundleResult.new(15, 'L09') }
+  describe '#initialize' do
+    subject { BundleResult.new(15, 'L09') }
 
-  it { expect(subject.total_flowers).to eq 15 }
-  it { expect(subject.flower_code).to eq 'L09' }
-  it { expect(subject.remaining_flowers).to eq 15 }
+    it { expect(subject.total_flowers).to eq 15 }
+    it { expect(subject.flower_code).to eq 'L09' }
+    it { expect(subject.remaining_flowers).to eq 15 }
+    it { expect(subject.bundles.empty?).to be_truthy }
+  end
+
+  describe '#create_bundle and #can_create_bundle?' do
+    let(:bundle_rule) { BundleRule.new(4, 6.99) }
+
+    subject { BundleResult.new(10, 'L09') }
+
+    context 'when there is no bundle yet' do
+      it { expect(subject.can_create_bundle?(bundle_rule)).to be_truthy }
+    end
+
+    context 'when there is enough remaining flowers and bundle once' do
+      before do
+        subject.create_bundle(bundle_rule)
+      end
+
+      it 'should decrease corresponding remaining flowers' do
+        expect(subject.remaining_flowers).to eq 10 - 4
+      end
+
+      it 'should have correct bundles' do
+        expect(subject.bundles).to eq(bundle_rule => 1)
+      end
+
+      it { expect(subject.all_bundled?).to be_falsy }
+    end
+
+    context 'when there is enough remaining flowers and bundle twice' do
+      before do
+        subject.create_bundle(bundle_rule)
+        subject.create_bundle(bundle_rule)
+      end
+
+      it 'should decrease corresponding remaining flowers' do
+        expect(subject.remaining_flowers).to eq 10 - 4 * 2
+      end
+
+      it 'should have correct bundles' do
+        expect(subject.bundles).to eq(bundle_rule => 2)
+      end
+
+      it { expect(subject.all_bundled?).to be_falsy }
+    end
+
+    context 'when there is no enough remaining flowers' do
+      before do
+        subject.create_bundle(bundle_rule)
+        subject.create_bundle(bundle_rule)
+      end
+
+      it { expect(subject.can_create_bundle?(bundle_rule)).to be_falsy }
+
+      it 'should raise ArgumentError' do
+        expect { subject.create_bundle }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'create_bundle for different bundle rule' do
+      let(:bundle_rule2) { BundleRule.new(2, 2.99) }
+
+      before do
+        subject.create_bundle(bundle_rule)
+        subject.create_bundle(bundle_rule)
+        subject.create_bundle(bundle_rule2)
+      end
+
+      it { expect(subject.remaining_flowers).to eq 0 }
+
+      it { expect(subject.all_bundled?).to be_truthy }
+
+      it 'should have correct bundles' do
+        expect(subject.bundles).to eq(bundle_rule => 2, bundle_rule2 => 1)
+      end
+    end
+  end
 end
