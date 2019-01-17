89300b4d64baf3008382bca132a92211a0edd264
andy.wang <andy.wang@blockchaintech.net.au>
2019-01-17 22:38:01 +1100

    Make Flower as an ActiveRecord, add .find method

--- lib/flower.rb
+++ lib/flower.rb
@@ -1,3 +1,5 @@
+require_relative 'bundle_rule'
+
 class Flower
   attr_reader :name, :code, :bundle_rules
 
@@ -5,5 +7,17 @@ class Flower
     @name = name
     @code = code
     @bundle_rules = bundle_rules.sort { |a, b| b.price <=> a.price }
+  end
+
+  @all_flowers = { 'R12' => Flower.new('Roses', 'R12', [BundleRule.new(5, 6.99),
+                                                        BundleRule.new(10, 12.99)]),
+                   'L09' => Flower.new('Lilies', 'L09', [BundleRule.new(3, 9.95),
+                                                         BundleRule.new(6, 16.95),
+                                                         BundleRule.new(9, 24.95)]),
+                   'T58' => Flower.new('Tulips', 'T58', [BundleRule.new(3, 5.95),
+                                                         BundleRule.new(5, 9.95),
+                                                         BundleRule.new(9, 16.99)]) }
+  def self.find(code)
+    @all_flowers.fetch(code) { |code| raise ArgumentError, "Code #{} is not found" }
   end
 end
--- spec/unit/flower_spec.rb
+++ spec/unit/flower_spec.rb
@@ -2,20 +2,37 @@ require 'flower'
 require 'bundle_rule'
 
 describe Flower do
-  subject do
-    Flower.new('Lilies', 'L09', [
-                 BundleRule.new(6, 16.95),
-                 BundleRule.new(3, 9.95),
-                 BundleRule.new(9, 24.95)
-               ])
+  describe '#initialize' do
+    subject do
+      Flower.new('Lilies', 'L09', [
+                  BundleRule.new(6, 16.95),
+                  BundleRule.new(3, 9.95),
+                  BundleRule.new(9, 24.95)
+                ])
+    end
+
+    it { expect(subject.name).to eq 'Lilies' }
+    it { expect(subject.code).to eq 'L09' }
+
+    it 'should sort the bundle_rules by price descending order' do
+      expect(subject.bundle_rules).to eq [BundleRule.new(9, 24.95),
+                                          BundleRule.new(6, 16.95),
+                                          BundleRule.new(3, 9.95)]
+    end
   end
+  
+  describe '.find' do
+    context 'when the code exists' do
+      it 'should return correct flower' do
+        flower = Flower.find('R12')
+        expect(flower.name).to eq 'Roses'
+        expect(flower.code).to eq 'R12'
+        expect(flower.bundle_rules).to eq [BundleRule.new(10, 12.99), BundleRule.new(5, 6.99)]
+      end
+    end
 
-  it { expect(subject.name).to eq 'Lilies' }
-  it { expect(subject.code).to eq 'L09' }
-
-  it 'should sort the bundle_rules by price descending order' do
-    expect(subject.bundle_rules).to eq [BundleRule.new(9, 24.95),
-                                        BundleRule.new(6, 16.95),
-                                        BundleRule.new(3, 9.95)]
+    context 'when the code does not exist' do
+      it { expect { Flower.find('NON') }.to raise_error(ArgumentError) }
+    end
   end
 end
